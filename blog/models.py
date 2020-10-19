f6d06d1d9f60445730156ccd671d2a765149f2e0
Saul Feliz <646634@gmail.com>
2020-10-19 09:50:47 -0500

    created category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
