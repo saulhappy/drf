66ca4510e11c5a3a30bfce56757b832252b4331f
saulhappy <646634@gmail.com>
2022-03-15 14:54:09 -0500

    add products app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
+    body = request.body
+    print(body)
+    print(request.GET)
     return JsonResponse({"message": "hey, saul"})
 
-    
