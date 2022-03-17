a64da2ebd17da053e6b2aea2a7b5a86e0906cd9b
saulhappy <646634@gmail.com>
2022-03-17 08:28:29 -0500

    refactor using model_to_dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,9 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.all().order_by("?").first()
-    data = {}
     if random_product:
-        data["title"] = random_product.title
-        data["content"] = random_product.content
-        data["price"] = random_product.price
+        data = model_to_dict(random_product)
     return JsonResponse(data)
