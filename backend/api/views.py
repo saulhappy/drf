16e1b1bbb2f6d32bd08b805b36b933fe7557c04d
saulhappy <646634@gmail.com>
2022-03-18 09:49:55 -0500

    run black

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from products.models import Product
 
+
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.all().order_by("?").first()
     if random_product:
