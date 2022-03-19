0a00ebb389a5a79a7715b0ffecd9e2fa6be2db40
saulhappy <646634@gmail.com>
2022-03-19 12:05:11 -0500

    start with drf respone

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.all().order_by("?").first()
     if random_product:
         data = model_to_dict(random_product)
-    return JsonResponse(data)
+    return Response(data)
