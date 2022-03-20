11cf3e5fe6a34087bb59a85f3a24255c66a04217
saulhappy <646634@gmail.com>
2022-03-20 03:43:00 -0500

    getting started with serializers

--- backend/products/models.py
+++ backend/products/models.py
@@ -5,3 +5,10 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return f"{round(float(self.price) * .97, 2)}"
+
+    def get_discount(self):
+        return f"3%"
