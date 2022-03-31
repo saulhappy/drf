402148f6ee88814481e27487ad96f624f7a8e1db
saulhappy <646634@gmail.com>
2022-03-31 03:05:55 -0500

    add update detail view

--- backend/products/views.py
+++ backend/products/views.py
@@ -23,3 +23,17 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 
 product_list_create_view = ProductListCreateAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+product_update_view = ProductUpdateAPIView.as_view()
