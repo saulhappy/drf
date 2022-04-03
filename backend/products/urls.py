ac419033722591334d2c3165abfbb62bb94633dd
saulhappy <646634@gmail.com>
2022-04-02 22:22:24 -0500

    update urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.product_list_create_view),
     path("<int:pk>/", views.ProductDetailAPIView.as_view()),
-    path("<int:pk>/update", views.product_update_view),
-    # path("<int:pk>/delete", views.product_destroy_view),
+    path("<int:pk>/update/", views.product_update_view),
+    path("<int:pk>/delete/", views.product_delete_view),
 ]
