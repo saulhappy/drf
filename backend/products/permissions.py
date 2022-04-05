22787bfc711bbd86af98f1c76fac5763a8174ad5
saulhappy <646634@gmail.com>
2022-04-05 06:08:13 -0500

    printing out staff permissions

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -1,4 +1,9 @@
 from rest_framework import permissions
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
-    pass
+    def has_permission(self, request, view):
+        user = request.user
+        print(user.get_all_permissions())
+        if user.is_staff:
+            return True
+        return False
