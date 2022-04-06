219d872f36c9d94b2cac40d65e9795ff3a0c4053
saulhappy <646634@gmail.com>
2022-04-06 09:22:25 -0500

    update staff permissions

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -5,5 +5,6 @@ class IsStaffEditorPermission(permissions.DjangoModelPermissions):
         user = request.user
         print(user.get_all_permissions())
         if user.is_staff:
-            return True
+            if user.has_perm("products.view_product"):
+                return True
         return False
