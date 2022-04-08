ce487e4d1618d5dd044dd84ddc72546bbcf16b84
saulhappy <646634@gmail.com>
2022-04-08 10:41:03 -0500

    add authtoken endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import api_home
 
-urlpatterns = [path("", api_home)]
+urlpatterns = [
+    path("auth/", obtain_auth_token),
+    path("", api_home),
+]
