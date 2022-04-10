f3c7d711934e7892c1e77b28f9aedd1c98d64963
saulhappy <646634@gmail.com>
2022-04-10 06:50:54 -0500

    use auth token in list endpoint

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,20 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/products/"
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+auth_endpoint = "http://localhost:8000/api/auth/"
+password = getpass()
+
+
+auth_response = requests.post(auth_endpoint, json={"username": "saul", "password": password})
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()["token"]
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/"
+
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
