81824267175d104feebca5b1b70235b7657017c1
saulhappy <646634@gmail.com>
2022-04-13 07:38:47 -0500

    add routers

---
+++ backend/cfehome/routers.py
@@ -0,0 +1,8 @@
+from wsgiref.simple_server import demo_app
+from rest_framework.routers import DefaultRouter
+
+from products.viewsets import ProductViewSet
+
+router = DefaultRouter()
+
+router.register("products-2", ProductViewSet, basename="products")
