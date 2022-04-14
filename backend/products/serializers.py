732fe6d349d5266910e5a9e04805b65f5d4b996a
saulhappy <646634@gmail.com>
2022-04-14 18:13:37 -0500

    add get_url method; add to product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = [
-            "pk",
+            "url",
             "title",
             "content",
             "price",
             "sale_price",
             "my_discount",
         ]
+
+    def get_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk": obj.id}, request=request)
+
     def get_my_discount(self, obj):
         try:
             return obj.get_discount()
