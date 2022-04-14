aaf2d6a969090e6be71de6f62ad8d3bae8bab3c3
saulhappy <646634@gmail.com>
2022-04-14 18:15:53 -0500

    add pk to product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,6 +10,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            "pk",
             "url",
             "title",
             "content",
