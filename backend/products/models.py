51ef432014ce2eb76c7d3803087e523982591f02
saulhappy <646634@gmail.com>
2022-04-17 13:15:06 -0500

    add user to product model; add migration

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 
+User = settings.AUTH_USER_MODEL
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, default=1, null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
