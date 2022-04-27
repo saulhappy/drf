84a35aa97decd86a197d30b9adb6482ddf065610
saulhappy <646634@gmail.com>
2022-04-27 13:49:32 -0500

    starting out with getting other user products

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,3 +4,8 @@ from rest_framework import serializers
 class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
     id = serializers.IntegerField(read_only=True)
+    other_products = serializers.SerializerMethodField(read_only=True)
+
+    def get_other_products(self, user):
+        my_products = user.product_set.all()
+        return []
