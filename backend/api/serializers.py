72162089ae0b080c069ddbe7facc02bb43226ed4
saulhappy <646634@gmail.com>
2022-04-28 09:40:28 -0500

    add UserProductInlineSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 
 
+class UserProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk", read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
     id = serializers.IntegerField(read_only=True)
-    other_products = serializers.SerializerMethodField(read_only=True)
+    first_five_products = serializers.SerializerMethodField(read_only=True)
 
-    def get_other_products(self, user):
-        my_products = user.product_set.all()
-        return []
+    def get_first_five_products(self, user):
+        my_first_five_products_qs = user.product_set.all()[:5]
+        return UserProductInlineSerializer(
+            my_first_five_products_qs, many=True, context=self.context
+        ).data
