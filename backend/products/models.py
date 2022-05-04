61a049cb608c4d6a0304bf8424b284954a1c2cfb
saulhappy <646634@gmail.com>
2022-05-04 16:34:33 -0500

    add is_public field; make migrations

--- backend/products/models.py
+++ backend/products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    is_public = models.BooleanField(default=True)
 
     @property
     def sale_price(self):
