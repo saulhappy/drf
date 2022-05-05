89dcf1aefd82fd9cc7bba6d560d48c3c0798827f
saulhappy <646634@gmail.com>
2022-05-05 16:27:11 -0500

    add product search methods

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,14 +1,40 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
 
 User = settings.AUTH_USER_MODEL
+
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(is_public=True)
+
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().search(query, user=user)
+
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, default=1, null=True)
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
     is_public = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     @property
     def sale_price(self):
