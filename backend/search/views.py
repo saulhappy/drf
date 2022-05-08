8a596ea4c3f21ed9ad122c9022b21faee977819e
saulhappy <646634@gmail.com>
2022-05-08 06:53:22 -0500

    add serializer to search view

--- backend/search/views.py
+++ backend/search/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 
 from backend.products.models import Product
 
+from backend.products.serializers import ProductSerializer
 
 class SearchListView(generics.ListAPIView):
     queryset = Product.objects.all()
+    serilizer_class = ProductSerializer
