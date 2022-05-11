2e6b970b2cd2eea5e5e40b0257d66959ba3143d1
saulhappy <646634@gmail.com>
2022-05-11 09:40:50 -0500

    set initial qs results to None;

--- backend/search/views.py
+++ backend/search/views.py
@@ -4,15 +4,18 @@ from products.models import Product
 
 from products.serializers import ProductSerializer
 
+
 class SearchListView(generics.ListAPIView):
     queryset = Product.objects.all()
-    serilizer_class = ProductSerializer
+    serializer_class = ProductSerializer
 
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
-        q = self.request.GET.get('q')
-        user = None
-        if self.request.user.is_authenticated:
-            user = self.request.user
-        results = qs.search(q, user=user)
+        q = self.request.GET.get("q")
+        results = Product.objects.none()
+        if q:
+            user = None
+            if self.request.user.is_authenticated:
+                user = self.request.user
+            results = qs.search(q, user=user)
         return results
