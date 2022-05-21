22d0c381d3e56baae7ef8e00fdec0a42315b925c
saulhappy <646634@gmail.com>
2022-05-21 06:26:18 -0500

    make get_index method in search client

--- backend/search/client.py
+++ backend/search/client.py
@@ -4,5 +4,7 @@ from algoliasearch_django import algolia_engine
 def get_client():
     return algolia_engine.client
 
-
-client = get_client()
+def get_index(index_name="cfe_Product"):
+    client = get_client()
+    index = client.init_index(index_name)
+    return index
