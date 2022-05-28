d89079f609db0f88ad3549e97c1eed6d331a7dff
saulhappy <646634@gmail.com>
2022-05-28 11:13:31 -0500

    update view to use new filters

--- backend/search/views.py
+++ backend/search/views.py
@@ -6,14 +6,20 @@ from products.models import Product
 from products.serializers import ProductSerializer
 from search.client import perform_search
 
+
 class SearchListView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
+        user = None
+        if request.user.is_authenticated:
+            user = request.user.username
         query = request.GET.get("q")
         tag = request.GET.get("tag") or None
-        results = perform_search(query=query, tags=tag)
+        is_public = str(request.GET.get("is_public")) != "0"
+        results = perform_search(query=query, tags=tag, user=user, is_public=is_public)
         if results:
             return Response(results)
         return Response("", status=400)
+
 
 class SearchListViewOld(generics.ListAPIView):
     queryset = Product.objects.all()
