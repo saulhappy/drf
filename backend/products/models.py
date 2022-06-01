01c7436d95f8333556b5d14b352c89a5375be107
saulhappy <646634@gmail.com>
2022-06-01 11:44:23 -0500

    add path property to product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -32,14 +32,20 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, default=1, null=True)
-    title = models.CharField(max_length=120) # imagine we later want to call this "name", to the outside world, but want to leave it in the model...
+    title = models.CharField(
+        max_length=120
+    )  # imagine we later want to call this "name", to the outside world, but want to leave it in the model...
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
     is_public = models.BooleanField(default=True)
 
     objects = ProductManager()
 
-    #exposes "title" as "name"
+    @property
+    def path(self):
+        return f"/products/{self.pk}/"
+
+    # exposes "title" as "name"
     @property
     def name(self):
         return self.title
